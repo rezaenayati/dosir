fa7b408fd2bf42928998f5a44a91cd08133354bc
Reza Enayati <enayatii.reza@gmail.com>
2019-10-23 17:23:49 +0330

    Right-to-left for Register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,8 +3,13 @@ import {
     Container, Link, Checkbox, Grid , Paper, TextField, Avatar, Button, FormControlLabel    
 } from "@material-ui/core";
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import '../App.css';
+
+const theme = createMuiTheme({
+    direction: 'rtl', 
+});
 
 export default class Register extends React.Component{
     render(){
@@ -12,34 +17,36 @@ export default class Register extends React.Component{
             <Grid container component="main" style={{height: '100vh'}}>
                 <Grid item xs={false} sm={4} md={7} style={{backgroundSize: 'cover', backgroundImage: 'url(https://wallpaperplay.com/walls/full/9/5/e/13971.jpg)'}} />
                 <Grid xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                    <form style={{margin: 100}}>
-                        <div>
+                    <form dir="rtl" style={{margin: 100}}>
+                        <div dir="rtl">
                             <LockOutlinedIcon style={{color: 'red'}} />
                             <h1>ثبت نام</h1>
                         </div>
+                        <ThemeProvider theme={theme}>
                         <TextField
-                            label='شماره موبایل'
-                            style={{marginTop: 10}}
-                            fullWidth
-                            variant="outlined"
-                            autoComplete='PhoneNumber'
-                            autoFocus
-                        />
-                        <TextField
-                            label='ایمیل'
-                            style={{marginTop: 10}}
+                            placeholder='ایمیل'
+                            style={{direction: 'rtl', marginTop: 10}}
                             fullWidth
                             variant="outlined"
                             autoComplete='email'
+                            autoFocus                            
+                        />
+                        <TextField
+                            placeholder='رمز عبور'
+                            style={{marginTop: 10}}
+                            fullWidth
+                            variant="outlined"
+                            autoComplete='password'
                             autoFocus
                         />
                         <TextField
-                            label='رمز عبور'
+                            placeholder='تایید رمز عبور'
                             style={{marginTop: 10}}
                             fullWidth
                             variant="outlined"
                             autoFocus
                         />
+                        </ThemeProvider>
                         <FormControlLabel
                             control={<Checkbox value="remember" color="primary" />}
                             label="مرا به خاطر داشته باش"
@@ -47,7 +54,7 @@ export default class Register extends React.Component{
                         <Button fullWidth style={{color: 'white', marginTop: 10, backgroundColor: 'blue'}}>
                             ثبت نام
                         </Button>
-                        <Link style={{marginTop: 10}} href="#" variant="body2">
+                        <Link style={{marginTop: 20}} href="#" variant="body2">
                         ورود به حساب
                         </Link>
                     </form>
