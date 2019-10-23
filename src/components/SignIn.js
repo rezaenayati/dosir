f70282d0207b4782c21f640cf42f77a43621b3ff
Reza Enayati <enayatii.reza@gmail.com>
2019-10-23 18:35:52 +0330

    implement SignUp and SignIn components in Register page

---
+++ src/components/SignIn.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { 
+    Checkbox, TextField, Button, FormControlLabel    
+} from "@material-ui/core";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import '../App.css';
+
+const theme = createMuiTheme({
+    direction: 'rtl', 
+});
+
+
+
+export default class SignIn extends React.Component{
+
+    render(){
+        return(
+            <form dir="rtl" style={{marginTop: 100, marginLeft: 100, marginRight: 100}}>
+            <div dir="rtl">
+                <LockOutlinedIcon style={{color: 'red'}} />
+                <h1>ورود</h1>
+            </div>
+            <ThemeProvider theme={theme}>
+            <TextField
+                placeholder='ایمیل'
+                style={{marginTop: 10}}
+                fullWidth
+                variant="outlined"
+                autoComplete='email'
+                autoFocus                            
+            />
+            <TextField
+                placeholder='رمز عبور'
+                style={{marginTop: 10}}
+                fullWidth
+                variant="outlined"
+                autoComplete='password'
+                autoFocus
+            />
+            </ThemeProvider>
+            <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="مرا به خاطر داشته باش"
+            />
+            <Button fullWidth style={{color: 'white', marginTop: 10, backgroundColor: 'blue'}}>
+                ورود
+            </Button>
+        </form>
+
+        );
+    }
+
+}
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,63 +1,38 @@
 import React from 'react';
 import { 
-    Container, Link, Checkbox, Grid , Paper, TextField, Avatar, Button, FormControlLabel    
+    AppBar, Tab, Tabs, Container, Link, Checkbox, Grid , Paper, TextField, Avatar, Button, FormControlLabel    
 } from "@material-ui/core";
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import '../App.css';
-
-const theme = createMuiTheme({
-    direction: 'rtl', 
-});
+import SignUp from '../components/SignUp';
+import SignIn from '../components/SignIn';
 
 export default class Register extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            signUp: true,
+        }
+    }
     render(){
+        const reverseSignUp = !this.state.signUp;
         return( 
             <Grid container component="main" style={{height: '100vh'}}>
                 <Grid item xs={false} sm={4} md={7} style={{backgroundSize: 'cover', backgroundImage: 'url(https://wallpaperplay.com/walls/full/9/5/e/13971.jpg)'}} />
                 <Grid xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                    <form dir="rtl" style={{margin: 100}}>
-                        <div dir="rtl">
-                            <LockOutlinedIcon style={{color: 'red'}} />
-                            <h1>ثبت نام</h1>
-                        </div>
-                        <ThemeProvider theme={theme}>
-                        <TextField
-                            placeholder='ایمیل'
-                            style={{direction: 'rtl', marginTop: 10}}
-                            fullWidth
-                            variant="outlined"
-                            autoComplete='email'
-                            autoFocus                            
-                        />
-                        <TextField
-                            placeholder='رمز عبور'
-                            style={{marginTop: 10}}
-                            fullWidth
-                            variant="outlined"
-                            autoComplete='password'
-                            autoFocus
-                        />
-                        <TextField
-                            placeholder='تایید رمز عبور'
-                            style={{marginTop: 10}}
-                            fullWidth
-                            variant="outlined"
-                            autoFocus
-                        />
-                        </ThemeProvider>
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="مرا به خاطر داشته باش"
-                        />
-                        <Button fullWidth style={{color: 'white', marginTop: 10, backgroundColor: 'blue'}}>
-                            ثبت نام
-                        </Button>
-                        <Link style={{marginTop: 20}} href="#" variant="body2">
-                        ورود به حساب
-                        </Link>
+                    <form>
                     </form>
+                    {this.state.signUp&&<SignUp />}
+                    {!this.state.signUp&&<SignIn />}
+                    <div dir="rtl" style={{marginTop: 10}}>
+                        <Link
+                            style={{color: 'blue', marginTop: 10 , marginRight: 100}}
+                            onClick={() => {this.setState({signUp: reverseSignUp})}}>
+                                {!reverseSignUp&&"ورود به حساب"}
+                                {reverseSignUp&&"ایجاد حساب جدید"}
+                        </Link>
+                    </div>
                 </Grid>
             </Grid>
         );
