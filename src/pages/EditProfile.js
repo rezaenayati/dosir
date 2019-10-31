eda3ce9226d7c83b4181a7abc5d6c3e30db61b07
Reza Enayati <enayatii.reza@gmail.com>
2019-10-31 06:56:26 +0330

    save doctor info at state and check auth in edit profile page

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,30 +1,60 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Avatar } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
 
 const mapStateToProps = state => ({ 
     ...state, 
     email: state.auth.email,
     authenticated: state.auth.authenticated,
+    doctor: state.currentUser.doctor
 });
 
 const mapDispatchToProps = dispatch => ({
-// addDoctor: ({}) => {}
 });
 
 class EditProfile extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            doctor: {
+
+            }
+        }
+    }
+
+    componentDidMount(){
+        this.setState({doctor: this.props.doctor});
+        if(!this.props.authenticated) 
+            this.props.history.push('/');    
+
+    }
+
     render(){
         return(
             <div style={{}}>
                 <p dir='rtl' style={styles.editProfileName}>ویرایش پروفایل</p>
+                <Avatar align='center'
+                        src={this.state.doctor.image} 
+                        style={styles.avatar}  
+                                />
             </div>
         );
     }
 }
 
 const styles = {
+    avatar: {
+        marginTop: 20, 
+        marginLeft: 'auto', 
+        marginRight: 'auto', 
+        width: 100, 
+        height:100
+    },
     editProfileName:{
         fontFamily: 'Vazir',
     }
 }
 
-export default connect(mapStateToProps , mapDispatchToProps)(EditProfile);
+export default withRouter(connect(mapStateToProps , mapDispatchToProps)(EditProfile));
