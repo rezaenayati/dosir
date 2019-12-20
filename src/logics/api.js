c12b215fa7d66a8b87e6a69237972681ca480955
Reza Enayati <enayatii.reza@gmail.com>
2019-12-21 01:59:12 +0330

    change Url to server's ip

--- src/logics/api.js
+++ src/logics/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const mainUrl = "http://192.168.6.177:8000/api/v1/";
+const mainUrl = "http://185.209.243.115/api/v1/";
 
 export const createDoctor = async (phone, password, name, family) => {
 
@@ -107,3 +107,81 @@ export const editDoctorInfo = async (access , doctor , editedDoctor) => {
 
 }
 
+
+
+
+
+
+
+
+
+
+
+// export const fetchPatientInfo = async (access , number) => {
+//     console.log("axios");
+//     console.log(access);
+//     console.log(number);
+    
+//     var data = JSON.stringify({
+//         "phone_num": "+989123555555"
+//     });      
+
+//     console.log(data);
+    
+//     return new Promise((resolve, reject) => {
+//         axios({
+//             method: 'get',
+//             url: `${mainUrl}report/doctor/get-patient/`,
+//             // crossDomain: true,
+//             // withCredentials : true,
+//             data: data,
+//             headers:{
+//                 'Authorization': `Bearer ${access}`,
+//                 'Content-Type':'application/json',
+//                 'Accept': '*/*',
+//                 'Access-Control-Allow-Origin': '*',
+//                 'Access-Control-Allow-Methods': "POST, GET, PUT, OPTIONS, PATCH, DELETE",
+//                 // 'Accept-Encoding' : 'gzip, deflate',
+//                 // 'Content-Length' : '33',
+//                 // 'Connection' : 'keep-alive',
+//                 'Cache-Control' : 'no-cache'   
+//             }
+//         })
+//         .then(response => resolve(response.data) )
+//         .catch((er) => reject(er))
+//     });
+// }
+
+
+// export const fetchPatientInfoxfdgchjk = async (access , number) => {
+
+//     console.log("axios");
+//     var body = JSON.stringify({
+//         "phone_num": "+989123555555"
+//     });      
+
+//     // console.log(phone);
+    
+//     return new Promise((resolve, reject) => {
+//         axios({
+//             method: 'get',
+//             url: `${mainUrl}report/doctor/get-patient/`,
+//             data: body,
+//             crossDomain: true,
+//             withCredentials : true,
+//             headers:{
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json',
+//                 'Access-Control-Allow-Origin': 'http://localhost:3000/# http://192.168.6.177:8000',
+//                 'Access-Control-Allow-Credentials': true,
+//                 'Access-Control-Allow-Methods': "POST, GET, PUT, OPTIONS, PATCH, DELETE",
+//                 'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type'
+
+//                 // 'Origin' : 'http://192.168.6.177:8000/'
+//             }
+//         })
+//         .then(response => resolve(response.data) )
+//         .catch((er) => reject(er))
+//     });
+
+// }
