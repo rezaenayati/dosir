ef124b412ac6df3138e4931fd17ef23381bcbc5f
Reza Enayati <enayatii.reza@gmail.com>
2019-12-23 02:04:42 +0330

    Redesgin 404 page for mobile and desktop

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -9,13 +9,15 @@ export default class Page404 extends React.Component{
                 <header>
                     <Header />
                 </header>
-                <div 
-                    style={{
-                        height: "90vh",
-                        width: '190vh',
-                        backgroundImage:'url(http://uupload.ir/files/vbfe_undraw_page_not_found_su7k.png)',
-                        backgroundSize:'cover',
-                    }}>
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <img style={{
+                            maxHeight: window.innerHeight,
+                            maxWidth: (window.innerHeight <= window.innerWidth) ? window.innerWidth* 3/5 : window.innerWidth,
+                            marginRight: 'auto',
+                            marginLeft: 'auto'
+                        }} 
+                        src='http://uupload.ir/files/vbfe_undraw_page_not_found_su7k.png' />
+                    <p style={{marginRight: 'auto', marginLeft: 'auto', fontFamily: 'Vazir'}}> صفحه مورد نظر یافت نشد </p>    
                 </div>
             </div>
         );
